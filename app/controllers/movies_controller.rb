51c92628d3796d728217d4407dabc490d20cd387
Laurenzi <laurirutanen@gmail.com>
2018-09-28 17:58:29 +0000

    part 3 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,16 +12,61 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.ratings
-    @selected_ratings = params[:ratings]
-    puts "Valitut: #{@selected_ratings}"
-    @sort_column = params[:sort_column]
-    if @sort_column == 'title'
-      @title_class = 'hilite'
-      @release_date_class = ''
-    elsif @sort_column == 'release_date'
-      @title_class = ''
-      @release_date_class = 'hilite'
+    # Miten huomata tuleeko käyttäjä ekaa kertaa sivulle? => Jos session[:ratings], session[:sort_column] ja params[:ratings], params[:sort_column] ovat tyhjiä!
+    first_visit = session[:ratings].nil? and session[:sort_column].nil? and params[:ratings].nil? and params[:sort_column].nil?
+    if first_visit
+      puts "Eka kerta"
     end
+    
+    from_other_page = params[:ratings].nil? and params[:sort_column].nil? and (not first_visit)
+    if from_other_page 
+      puts "Muulta sivulta"
+      redirect_to movies_path(:ratings => session[:ratings], :sort_column => session[:sort_column])
+    else
+      if session[:ratings] != params[:ratings]
+        session[:ratings] = params[:ratings]
+        @selected_ratings = session[:ratings]
+      else
+        @selected_ratings = session[:ratings]
+      end
+      if session[:sort_column] != params[:sort_column]
+        session[:sort_column] = params[:sort_column]
+        @sort_column = session[:sort_column]
+      else 
+        @sort_column = session[:sort_column]
+      end
+    end
+    
+    
+    
+    # if not session[:sort_column].nil? and session[:sort_column].length != 0
+    #   @sort_column = session[:sort_column]
+    # else 
+    #   @sort_column = params[:sort_column]
+    # end
+    # puts "Selected Ratings: #{@selected_ratings}"
+    # puts "Session ratings: #{session[:ratings]} and params ratings: #{params[:ratings]}"
+    # if @sort_column == 'title'
+    #   @title_class = 'hilite'
+    #   @release_date_class = ''
+    # elsif @sort_column == 'release_date'
+    #   @title_class = ''
+    #   @release_date_class = 'hilite'
+    # end
+    # session[:ratings] = @selected_ratings
+    # session[:sort_column] = @sort_column
+  
+    # # What is the right condition to redirect?
+    # if @selected_ratings != params[:ratings]
+    #   puts "About to redirect to /movies"
+    #   #redirect_to movies_path(:ratings => session[:ratings], :sort_column => session[:sort_column])
+    # end
+    
+    # #if not (ratings_equal and sort_equal)
+    #   #puts "about to redirect"
+    #   #redirect_to movies_path(:ratings => session[:ratings], :sort_column => session[:sort_column])
+    # #end
+    
     
     @movies = Movie.order(@sort_column).where("rating IN (?)", @selected_ratings != nil ? @selected_ratings.keys : @all_ratings)
     
